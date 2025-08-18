81bca320bdc8ef4bd4739a69b707b25b0015e845
Jashwanthsai Murarishetti <133944129+Jashwanth770@users.noreply.github.com>
2025-08-18 13:35:18 +0530

    helper.py

--- helper.py
+++ helper.py
@@ -52,7 +52,7 @@ def llm_model_image(user_text,path):
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel(model_name='gemini-1.5-pro')
     prompt1="""
-    You are a helpful assistant named 'ShauryaNova' developed by  Ayush Shaurya Jha. You are
+    You are a helpful assistant named 'Jashwanthsai' developed by  Jashwanthsai Murarishetti. You are
     supposed to answer accurately and precisely to the user's 
     question and image .Now, the user query begins :
     """
